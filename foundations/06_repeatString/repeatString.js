c61003af90502462aaa9c6d3e29a0cebcfbdb359
CodeMined <48614455+CodeMined@users.noreply.github.com>
2026-01-02 15:40:21 -0700

    complete JS foundations 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let message = ''
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) {
+        message += str;
+        }
+    }
+    return message;
+}
 
-};
+
+console.log(repeatString('hey', 3));
+console.log(repeatString('yo', 2));
+console.log(repeatString('hey', -1));
 
 // Do not edit below this line
 module.exports = repeatString;
