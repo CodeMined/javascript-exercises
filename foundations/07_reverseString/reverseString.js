31bd4532547c6fc72f0655a1167280878d7a0659
CodeMined <48614455+CodeMined@users.noreply.github.com>
2026-01-02 16:06:12 -0700

    complete JS foundations 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// Another approach could be to loop over the string in reverse direction to construct a new one
+/*
+  const reverseString = function (string) {
+    let reversedString = "";
+
+    for (let i = string.length -1; i >= 0; i--) {
+      reversedString += string[i];
+    }
+    return reversedString;
+  };
+*/ 
