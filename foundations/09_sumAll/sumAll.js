febe4804be8a7eb9b18be9911761321dc110be21
CodeMined <48614455+CodeMined@users.noreply.github.com>
2026-01-02 21:24:43 -0700

    complete JS foundations 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+function sumAll(num1, num2) {
+    const sumArray = [];
+    if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            sumArray.push(i);
+        }
+        return sumArray.reduce((sum, n) => sum + n);
+    } else {
+        for (let i = num1; i <= num2; i++) {
+            sumArray.push(i);
+        }
+        return sumArray.reduce((sum, n) => sum + n);
+    }
 };
 
+console.log(sumAll(1, 4));
+console.log(sumAll(1, 6));
+console.log(sumAll(4, 5));
 // Do not edit below this line
 module.exports = sumAll;
